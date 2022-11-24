0a200e014c7f9eebb601df8e09f3de697f3707ad
jangsumin <jsm0726@naver.com>
2022-11-24 16:48:36 +0900

    feat: 캐릭터 모형 추가

--- src/Component/BridgeMaker.js
+++ src/Component/BridgeMaker.js
@@ -3,10 +3,11 @@ function BridgeMaker(props) {
   const listBridges = bridgeArray.map((number) => <li className="bridge"></li>);
 
   return (
-    <>
+    <div className="game-space">
+      <div className="character"></div>
       <ul className="up-bridge">{listBridges}</ul>
       <ul className="down-bridge">{listBridges}</ul>
-    </>
+    </div>
   );
 }
 
