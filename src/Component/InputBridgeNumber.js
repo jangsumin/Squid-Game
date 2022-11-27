cfdad979c967f63a6fefabca207e63ed029bab89
jangsumin <jsm0726@naver.com>
2022-11-27 17:37:32 +0900

    feat: useLocation 훅을 사용하여 값 전달받기

--- src/Component/InputBridgeNumber.js
+++ src/Component/InputBridgeNumber.js
@@ -1,15 +1,26 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function InputBridgeNumber() {
+  const [length, setLength] = useState(0);
   const navigate = useNavigate();
+
+  const saveLength = (e) => {
+    setLength(e.currentTarget.value);
+  };
+
   const navigateToGame = () => {
-    navigate('/game');
+    navigate('/game', {
+      state: {
+        bridgeLength: length,
+      }
+    });
   };
 
   return (
     <form class="input-form">
       <label>
-        <input class="input-txt" type="text" name="numberOfBridges" placeholder="5에서 8까지의 숫자를 입력하고" />
+        <input class="input-txt" type="text" name="numberOfBridges" placeholder="5에서 8까지의 숫자를 입력하고" onChange={saveLength}/>
       </label>
       <input class="input-btn" type="submit" value="눌러!" onClick={navigateToGame} />
     </form>
