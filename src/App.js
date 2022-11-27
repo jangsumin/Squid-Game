01d375aa84c468b877db97ec6b10e78fcf6743f1
jangsumin <jsm0726@naver.com>
2022-11-27 15:00:41 +0900

    feat: 메인 페이지에 게임 시작 버튼만 있도록 구현

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import StartGame from "./Component/StartGame";
 import InputBridgeNumber from "./Component/InputBridgeNumber";
-import BridgeMaker from "./Component/BridgeMaker";
-import Move from "./Component/Move";
-import RetryOrQuit from "./Component/RetryOrQuit";
+// import BridgeMaker from "./Component/BridgeMaker";
+// import Move from "./Component/Move";
+// import RetryOrQuit from "./Component/RetryOrQuit";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <StartGame />
-      <InputBridgeNumber />
-      <BridgeMaker numberOfBridges={5} />
-      <Move />
-      <RetryOrQuit />
+    <div class="app">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<StartGame />}></Route>
+          <Route path="/input" element={<InputBridgeNumber />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
