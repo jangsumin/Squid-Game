5327b287ede85f92f00d337fb2350c1228439ced
jangsumin <jsm0726@naver.com>
2022-11-27 17:26:45 +0900

    feat: useNavigate를 통해 실제 게임 페이지로 이동

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import StartGame from "./Component/StartGame";
 import InputBridgeNumber from "./Component/InputBridgeNumber";
 // import BridgeMaker from "./Component/BridgeMaker";
 // import Move from "./Component/Move";
-// import RetryOrQuit from "./Component/RetryOrQuit";
+import RetryOrQuit from "./Component/RetryOrQuit";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<StartGame />}></Route>
           <Route path="/input" element={<InputBridgeNumber />}></Route>
+          <Route path="/game" element={<RetryOrQuit />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/Component/InputBridgeNumber.js
+++ src/Component/InputBridgeNumber.js
@@ -1,10 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
 function InputBridgeNumber() {
+  const navigate = useNavigate();
+  const navigateToGame = () => {
+    navigate('/game');
+  };
+
   return (
     <form class="input-form">
       <label>
-        <input class="input-txt" type="text" name="numberOfBridges" placeholder="5에서 8까지의 숫자를 입력하고"/>
+        <input class="input-txt" type="text" name="numberOfBridges" placeholder="5에서 8까지의 숫자를 입력하고" />
       </label>
-      <input class="input-btn" type="submit" value="눌러!" />
+      <input class="input-btn" type="submit" value="눌러!" onClick={navigateToGame} />
     </form>
   );
 }
