5b7a1d73114b4da9e999b0752201f9dd89c93661
jangsumin <jsm0726@naver.com>
2022-12-01 13:49:51 +0900

    feat: add squid character

--- src/Component/BridgeMaker.js
+++ src/Component/BridgeMaker.js
@@ -1,10 +1,14 @@
+import squid from '../assets/squid.png';
+
 function BridgeMaker(props) {
   const bridgeArray = Array(props.numberOfBridges).fill(0);
   const listBridges = bridgeArray.map((number) => <li className="bridge"></li>);
 
   return (
     <div className="game-space">
-      {/* <div className="character"></div> */}
+      <div>
+        <img className="character" src={squid} alt=""></img>
+      </div>
       <div>
         <ul className="up-bridge">{listBridges}</ul>
         <ul className="down-bridge">{listBridges}</ul>
