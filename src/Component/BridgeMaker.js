100746a87890996306d880a791192a89142d3621
jangsumin <jsm0726@naver.com>
2022-12-01 11:53:21 +0900

    design: create up & down bridge and rotate slightly

--- src/Component/BridgeMaker.js
+++ src/Component/BridgeMaker.js
@@ -4,8 +4,8 @@ function BridgeMaker(props) {
 
   return (
     <div className="game-space">
-      <div className="character"></div>
-      <div className="end-line"></div>
+      {/* <div className="character"></div>
+      <div className="end-line"></div> */}
       <ul className="up-bridge">{listBridges}</ul>
       <ul className="down-bridge">{listBridges}</ul>
     </div>
