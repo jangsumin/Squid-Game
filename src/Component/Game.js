19ac1cdf5a982a8d49cb12ceabf8aa7cf5bab462
jangsumin <jsm0726@naver.com>
2022-12-01 15:45:39 +0900

    refactor: remove sub-components in the game component

--- src/Component/Game.js
+++ src/Component/Game.js
@@ -1,17 +1,42 @@
-import BridgeMaker from "./BridgeMaker";
-import Move from "./Move";
-import RetryOrQuit from "./RetryOrQuit";
+import answerBridgeMaker from './answerBridgeMaker';
+import squid from '../assets/squid.png';
 import { useLocation } from 'react-router-dom';
 
 function Game() {
   const location = useLocation();
   const bridgeLength = location.state.bridgeLength;
 
+  const bridgeArray = Array(Number(bridgeLength)).fill(0);
+  const listBridges = bridgeArray.map((number, idx) => <li key={idx} className="bridge"></li>);
+
+  const answerBridge = answerBridgeMaker.makeAnswerBridge(Number(bridgeLength));
+  const bridge = [];
+
+  const saveUpBridge = () => {
+    bridge.push(1);
+    console.log(bridge);
+  };
+  
+  const saveDownBridge = () => {
+    bridge.push(0);
+    console.log(bridge);
+  };
+
   return (
     <>
-      <BridgeMaker numberOfBridges={Number(bridgeLength)}/>
-      <Move numberOfBridges={Number(bridgeLength)}/>
-      {/* <RetryOrQuit /> */}
+      <div className="game-space">
+        <img className="character" src={squid} alt=""></img>
+        <div className='start-line'></div>
+        <div>
+          <ul className="up-bridge">{listBridges}</ul>
+          <ul className="down-bridge">{listBridges}</ul>
+        </div>
+        <div className="end-line">도착</div>
+      </div>
+      <div className="button-space">
+        <button className="up-button" onClick={saveUpBridge}>UP!</button>
+        <button className="down-button" onClick={saveDownBridge}>DOWN!</button>
+      </div>
     </>
   );
 };
