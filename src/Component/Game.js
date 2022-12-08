e18d21a226d673c368ed0c0a551f594429036b1f
jangsumin <jsm0726@naver.com>
2022-12-08 16:50:14 +0900

    feat: 오징어 이동 기능 구현

--- src/Component/Game.js
+++ src/Component/Game.js
@@ -1,31 +1,40 @@
 import answerBridgeMaker from './answerBridgeMaker';
 import squid from '../assets/squid.png';
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 function Game() {
+  // 이전 페이지에서 입력 받은 다리의 길이를 가져오기
   const location = useLocation();
   const bridgeLength = location.state.bridgeLength;
 
+  // 입력 받은 다리의 개수만큼 생성
   const bridgeArray = Array(Number(bridgeLength)).fill(0);
   const listBridges = bridgeArray.map((number, idx) => <li key={idx} className="bridge"></li>);
 
+  // 정답인 다리와 내가 입력할 다리의 배열 생성
   const answerBridge = answerBridgeMaker.makeAnswerBridge(Number(bridgeLength));
   const bridge = [];
 
-  const saveUpBridge = () => {
+  const [squidTop, setSquidTop] = useState(110);
+  const [squidLeft, setSquidLeft] = useState(5);
+
+  const clickUpBtn = () => {
     bridge.push(1);
-    console.log(bridge);
-  };
-  
-  const saveDownBridge = () => {
+    setSquidTop(45);
+    setSquidLeft(squidLeft + 60);
+  }
+
+  const clickDownBtn = () => {
     bridge.push(0);
-    console.log(bridge);
-  };
+    setSquidTop(185);
+    setSquidLeft(squidLeft + 60);
+  }
 
   return (
     <>
       <div className="game-space">
-        <img className="character" src={squid} alt=""></img>
+        <img className="character" src={squid} alt="" style={{top: `${squidTop}px`, left: `${squidLeft}px`}}></img>
         <div className='start-line'></div>
         <div>
           <ul className="up-bridge">{listBridges}</ul>
@@ -34,8 +43,8 @@ function Game() {
         <div className="end-line">도착</div>
       </div>
       <div className="button-space">
-        <button className="up-button" onClick={saveUpBridge}>UP!</button>
-        <button className="down-button" onClick={saveDownBridge}>DOWN!</button>
+        <button className="up-button" onClick={clickUpBtn}>UP!</button>
+        <button className="down-button" onClick={clickDownBtn}>DOWN!</button>
       </div>
     </>
   );
