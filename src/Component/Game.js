ff055c130472c854e7e2bbe0a09b5f5878dc3a43
jangsumin <jsm0726@naver.com>
2022-12-08 17:41:51 +0900

    fix: useEffect 사용하여 비동기 처리 방지

--- src/Component/Game.js
+++ src/Component/Game.js
@@ -1,7 +1,7 @@
 import answerBridgeMaker from './answerBridgeMaker';
 import squid from '../assets/squid.png';
 import { useLocation } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Game() {
   // 이전 페이지에서 입력 받은 다리의 길이를 가져오기
@@ -14,22 +14,35 @@ function Game() {
 
   // 정답인 다리와 내가 입력할 다리의 배열 생성
   const answerBridge = answerBridgeMaker.makeAnswerBridge(Number(bridgeLength));
-  const bridge = [];
+  const [bridge, setBridge] = useState([]);
 
+  // 처음 오징어 캐릭터의 시작 위치는 top: 110px 그리고 left: 5px
   const [squidTop, setSquidTop] = useState(110);
   const [squidLeft, setSquidLeft] = useState(5);
 
+  // UP 버튼을 누르면 오징어가 위, 오른쪽으로 이동, 그리고 bridge 업데이트
   const clickUpBtn = () => {
-    bridge.push(1);
+    setBridge([...bridge, 1]);
     setSquidTop(45);
     setSquidLeft(squidLeft + 60);
   }
 
+  // DOWN 버튼을 누르면 오징어가 아래, 오른쪽으로 이동, 그리고 bridge 업데이트
   const clickDownBtn = () => {
-    bridge.push(0);
+    setBridge([...bridge, 0]);
     setSquidTop(185);
     setSquidLeft(squidLeft + 60);
   }
+
+  // useEffect 훅 사용해서 비동기 처리 방지
+  const [checkRender, setCheckRender] = useState(false);
+  useEffect(() => {
+    if (checkRender) {
+      console.log("업데이트");
+      console.log(bridge);
+    }
+    setCheckRender(true);
+  }, [bridge]);
 
   return (
     <>
